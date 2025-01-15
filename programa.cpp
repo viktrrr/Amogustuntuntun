aaa74b48e258edd72d6f510bb772bf20e8468f51
karinaivanova123 <karina.ivanova2107@gmail.com>
2025-01-15 09:34:43 +0200

    Update programa.cpp

--- programa.cpp
+++ programa.cpp
@@ -7,7 +7,7 @@ int main() {
   char op;
   float num1, num2; //skaičiai gali buti įvairus
 
-  cout << "Parašykite operatorių: +, -, *, /: "; //Rekia pasirinkti ženklą sprendimui
+  cout << "Parašykite operatorių: +, -, *, / "; //Rekia pasirinkti ženklą sprendimui
   cin >> op;
 
   cout << "Parašykite skaičius: "; //Reikia parašyti sprendinį
@@ -31,13 +31,9 @@ int main() {
       cout << num1 << " / " << num2 << " = " << num1 / num2;  //Jeigu operatorius yra "/", tada parašytas toks sprendimas
       break;
 
-    case ':':
-      cout << num1 << " : " << num2 << " = " << num1 : num2;  //Jeigu operatorius yra ":", tada parašytas toks sprendimas
-      break;
-
     default:
 
-      cout << "Klaida! Operatorius parašytas netaisyklingai"; //Jeigu operatorius yra ne "-, +, *, :/", tada bus klaida
+      cout << "Klaida! Operatorius parašytas netaisyklingai"; //Jeigu operatorius yra ne "-, +, *, /", tada bus klaida
       break;
   }
 
