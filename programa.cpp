406d938034199d8d6faafcf112d88cbf2e25b3ea
evelina10-1 <evelina.mihaduk@gmail.com>
2025-01-15 09:29:41 +0200

    Update programa.cpp

--- programa.cpp
+++ programa.cpp
@@ -1,38 +1,39 @@
+//kalkuliatorius 
 # include <iostream>
 using namespace std;
 
 int main() {
 
   char op;
-  float num1, num2;
+  float num1, num2; //skaičiai gali buti įvairus
 
-  cout << "Parašykite operatorių: +, -, *, /: ";
+  cout << "Parašykite operatorių: +, -, *, /: "; //Rekia pasirinkti ženklą sprendimui
   cin >> op;
 
-  cout << "Parašykite skaičius: ";
+  cout << "Parašykite skaičius: "; //Reikia parašyti sprendinį
   cin >> num1 >> num2;
 
   switch(op) {
 
     case '+':
-      cout << num1 << " + " << num2 << " = " << num1 + num2;
+      cout << num1 << " + " << num2 << " = " << num1 + num2; //Jeigu operatorius yra "+", tada parašytas toks sprendimas
       break;
 
     case '-':
-      cout << num1 << " - " << num2 << " = " << num1 - num2;
+      cout << num1 << " - " << num2 << " = " << num1 - num2; //Jeigu operatorius yra "-", tada parašytas toks sprendimas
       break;
 
     case '*':
-      cout << num1 << " * " << num2 << " = " << num1 * num2;
+      cout << num1 << " * " << num2 << " = " << num1 * num2; //Jeigu operatorius yra "*", tada parašytas toks sprendimas
       break;
 
     case '/':
-      cout << num1 << " / " << num2 << " = " << num1 / num2;
+      cout << num1 << " / " << num2 << " = " << num1 / num2;  //Jeigu operatorius yra "/", tada parašytas toks sprendimas
       break;
 
     default:
 
-      cout << "Klaida! Operatorius parašytas netaisyklingai"; 
+      cout << "Klaida! Operatorius parašytas netaisyklingai"; //Jeigu operatorius yra ne "-, +, *, :", tada bus klaida
       break;
   }
 
